e0133f6e6336a9a4378bfe5f8d8160fc5f9374da
Parad0x763 <db726825@gmail.com>
2024-02-07 09:00:42 -0500

    Corrected whoLikesIt.js

--- JavaScript/whoLikesIt.js
+++ JavaScript/whoLikesIt.js
@@ -2,18 +2,18 @@
 
 function likes(names) {
    
-    if (names.length == 0) { //base case
+    if (names.length === 0) { //base case
       return "no one likes this";
     } else {
       let likeCount = 0;
       while (likeCount < names.length) {
         likeCount++;
       }
-      if (likeCount == 1) {
+      if (likeCount === 1) {
         return names[0] + " likes this";
-      } else if (likeCount == 2) {
+      } else if (likeCount === 2) {
         return names[0] + " and " + names[1] + " like this";
-      } else if (likeCount == 3) {
+      } else if (likeCount === 3) {
         let likes = names[0] + ", " + names[1] 
           + " and " + names[2] + " like this";
         return likes;
