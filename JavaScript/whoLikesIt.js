d0d0b6caa42059105fdd54e69c17af3f282d0b94
Parad0x763 <db726825@gmail.com>
2024-02-07 08:57:28 -0500

    JavaScript Who Likes it? Kata

---
+++ JavaScript/whoLikesIt.js
@@ -0,0 +1,24 @@
+/*JavaScript funciton to send who likes a post on button click*/
+
+function likes(names) {
+   
+    if (names.length == 0) { //base case
+      return "no one likes this";
+    } else {
+      let likeCount = 0;
+      while (likeCount < names.length) {
+        likeCount++;
+      }
+      if (likeCount == 1) {
+        return names[0] + " likes this";
+      } else if (likeCount == 2) {
+        return names[0] + " and " + names[1] + " like this";
+      } else if (likeCount == 3) {
+        let likes = names[0] + ", " + names[1] 
+          + " and " + names[2] + " like this";
+        return likes;
+      } else {
+        return names[0] + ", " + names[1] + " and " + (likeCount - 2) + " others like this";
+      }
+    }
+  }
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,5 @@
 Kata project ideas:
     https://github.com/gamontal/awesome-katas
+
+Codewars Kata projects:
+    https://www.codewars.com/dashboard
